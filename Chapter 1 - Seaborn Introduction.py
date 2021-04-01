6b7061f829eb21481bde5eae36263548dc654953
Lawless93 <lawless93@hotmail.com>
2021-04-01 16:10:41 +0100

    UCD Data Analytics - Intermediate Data Visualization with Seaborn

---
+++ Chapter 1 - Seaborn Introduction.py
@@ -0,0 +1,58 @@
+# Display a Seaborn distplot
+sns.distplot(df['Award_Amount'])
+plt.show()
+
+# Clear the distplot
+plt.clf()
+
+
+# Create a distplot
+sns.distplot(df['Award_Amount'],
+             kde=False,
+             bins=20)
+
+# Display the plot
+plt.show()
+
+
+# Create a distplot of the Award Amount
+sns.distplot(df['Award_Amount'],
+             hist=False,
+             rug=True,
+             kde_kws={'shade':True})
+
+# Plot the results
+plt.show()
+
+
+# Create a regression plot of premiums vs. insurance_losses
+sns.regplot(x="insurance_losses", y="premiums", data=df)
+
+# Display the plot
+plt.show()
+
+# Create an lmplot of premiums vs. insurance_losses
+sns.lmplot(x="insurance_losses", y="premiums", data=df)
+
+# Display the second plot
+plt.show()
+
+
+# Create a regression plot using hue
+sns.lmplot(data=df,
+           x="insurance_losses",
+           y="premiums",
+           hue="Region")
+
+# Show the results
+plt.show()
+
+
+# Create a regression plot with multiple rows
+sns.lmplot(data=df,
+           x="insurance_losses",
+           y="premiums",
+           row="Region")
+
+# Show the plot
+plt.show()
